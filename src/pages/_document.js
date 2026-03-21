c8ac3cf338bea616ebfc8c0b86d50b809f6f75c4
Ankit Singh <58619844+ankitshivmalan@users.noreply.github.com>
2026-03-21 13:45:37 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,11 @@ class MyDocument extends Document {
         <Head>
           <link rel='icon' type='image/png' href='/portfolio/ankit-formal.png' />
           <link rel='apple-touch-icon' href='/portfolio/ankit-formal.png' />
+          <script
+            async
+            src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3775988500417008'
+            crossOrigin='anonymous'
+          />
         </Head>
         <body>
           <Main />
